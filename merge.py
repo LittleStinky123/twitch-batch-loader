05813ae14d9836f1507080dffe11c0d7ad7deaf2
LittleStinky123 <arsch.kackewurst123@gmail.com>
2019-10-30 20:16:21 +0100

    Fix merge program to use unique entries

--- merge.py
+++ merge.py
@@ -6,15 +6,22 @@ f3 = open('diff.txt',"w")
 f4 = open('clips_final.txt',"w")
 lines = f.readlines()
 lines2 = f2.readlines()
-clip_list = lines.copy()
-diff_list = []
+clip_list = set()
+clip_list = clip_list.union(set(lines))
+clip_list = set(s.strip('\n') for s in clip_list)
+print(clip_list)
+diff_list = set()
 for line2 in lines2:
-    if line2 not in clip_list:
-        diff_list.append(line2)
-        clip_list.append(line2)
+    if line2.strip('\n') not in clip_list:
+        print(line2.strip('\n'))
+        diff_list.add(line2.strip('\n'))
+        clip_list.add(line2.strip('\n'))
 for entry in diff_list:
-    f3.write(entry)
+    newline = '\n' if '\n' not in entry else ''
+    print(entry)
+    f3.write(entry + newline)
 f3.close()
 for entry in clip_list:
-    f4.write(entry)
+    newline = '\n' if '\n' not in entry else ''
+    f4.write(entry + newline)
 f4.close()
